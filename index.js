aa7570f59d352972a1b57c56fac49f3813c74eb4
VitorPaulon <vtrpaulon@gmail.com>
2026-01-14 20:06:36 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,16 @@
+class hero {
+  constructor(name, type, health, attack, defense) {
+    this.name = name;
+    this.type = type;
+    this.health = health;
+    this.attack = attack;
+    this.defense = defense;
+  }
+    getInfo() {
+    return `Nome: ${this.name} \nTipo: ${this.type} \nVida: ${this.health} \nAtaque: ${this.attack} \nDefesa: ${this.defense}`;
+    }
+}
+
+const hero1 = new hero("Arus", "Guerreiro", 100, 20, 10);
+const hero2 = new hero("Jennica", "Maga", 80, 25, 5);
+const hero3 = new hero("Topapa", "Monge", 90, 15, 15);
